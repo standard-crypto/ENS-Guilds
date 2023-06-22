5d1c40198fb3ddec7f4c5c5032277e96408ae501
Gavi Galloway <gsgalloway@users.noreply.github.com>
2023-06-22 15:04:28 -0700

    feat: obey ENS wildcard resolver standard for non-addr records

--- contracts/ensWildcardResolvers/Erc721WildcardResolver.sol
+++ contracts/ensWildcardResolvers/Erc721WildcardResolver.sol
@@ -5,6 +5,7 @@ import "@ensdomains/ens-contracts/contracts/resolvers/profiles/IExtendedResolver
 import "@ensdomains/ens-contracts/contracts/utils/NameEncoder.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "../libraries/ENSParentName.sol";
@@ -13,6 +14,17 @@ contract Erc721WildcardResolver is IExtendedResolver, ERC165 {
     using ENSParentName for bytes;
     using ERC165Checker for address;
     using NameEncoder for string;
+    using Strings for string;
+    using Strings for address;
+    using Strings for uint256;
+
+    error RecordTypeNotSupported();
+
+    bytes4 public constant RESOLVER_SIGNATURE__ADDR = bytes4(keccak256(bytes("addr(bytes32)")));
+    bytes4 public constant RESOLVER_SIGNATURE__ADDR_WITH_COINTYPE = bytes4(keccak256(bytes("addr(bytes32,uint256)")));
+    bytes4 public constant RESOLVER_SIGNATURE__TEXT = bytes4(keccak256(bytes("text(bytes32,string)")));
+
+    uint256 private constant COIN_TYPE_ETH = 60;
 
     // dnsEncode(parentName) -> address
     // ex: key for "test.eth" is `0x04746573740365746800`
@@ -25,31 +37,90 @@ contract Erc721WildcardResolver is IExtendedResolver, ERC165 {
         tokens[encodedName] = IERC721(tokenContract);
     }
 
-    function resolve(bytes calldata name, bytes calldata) public view override returns (bytes memory) {
-        bytes memory emptyBytes;
+    function resolve(bytes calldata name, bytes calldata data) public view override returns (bytes memory) {
+        bytes4 resolutionFunction = bytes4(data[:4]);
 
-        // TODO: check the `data` param equals ABI encoding of addr(namehash(name))
+        if (resolutionFunction == RESOLVER_SIGNATURE__ADDR) {
+            address tokenOwner = _resolveEthAddr(name);
+            return abi.encode(tokenOwner);
+        } else if (resolutionFunction == RESOLVER_SIGNATURE__ADDR_WITH_COINTYPE) {
+            (, uint256 coinType) = abi.decode(data[4:], (bytes32, uint256));
+            if (coinType == COIN_TYPE_ETH) {
+                address tokenOwner = _resolveEthAddr(name);
+                return abi.encode(addressToBytes(tokenOwner));
+            } else {
+                // Unsupported COIN_TYPE
+                bytes memory emptyBytes;
+                return abi.encode(emptyBytes);
+            }
+        } else if (resolutionFunction == RESOLVER_SIGNATURE__TEXT) {
+            (, string memory key) = abi.decode(data[4:], (bytes32, string));
+            string memory result = _resolveTextRecord(name, key);
+            return abi.encode(result);
+        }
 
+        revert RecordTypeNotSupported();
+    }
+
+    function supportsInterface(bytes4 interfaceID) public view virtual override(ERC165) returns (bool) {
+        return interfaceID == type(IExtendedResolver).interfaceId || super.supportsInterface(interfaceID);
+    }
+
+    function _resolveEthAddr(bytes calldata name) private view returns (address) {
         (bytes calldata childUtf8Encoded, bytes calldata parentDnsEncoded) = name.splitParentChildNames();
 
         IERC721 tokenContract = tokens[parentDnsEncoded];
 
         // No NFT contract registered for this address
         if (address(tokenContract) == address(0)) {
-            return emptyBytes;
+            return address(0);
         }
 
         // Extract tokenId from child name
         (bool valid, uint256 tokenId) = _parseTokenIdFromName(childUtf8Encoded);
         if (!valid) {
-            return emptyBytes;
+            return address(0);
         }
-        address tokenOwner = tokenContract.ownerOf(tokenId);
-        return abi.encode(tokenOwner);
+
+        // Lookup token owner
+        address tokenOwner;
+        try tokenContract.ownerOf(tokenId) returns (address _tokenOwner) {
+            tokenOwner = _tokenOwner;
+        } catch {
+            tokenOwner = address(0);
+        }
+        return tokenOwner;
     }
 
-    function supportsInterface(bytes4 interfaceID) public view virtual override(ERC165) returns (bool) {
-        return interfaceID == type(IExtendedResolver).interfaceId || super.supportsInterface(interfaceID);
+    function _resolveTextRecord(bytes calldata name, string memory key) private view returns (string memory) {
+        (bytes calldata childUtf8Encoded, bytes calldata parentDnsEncoded) = name.splitParentChildNames();
+
+        IERC721 tokenContract = tokens[parentDnsEncoded];
+
+        // No NFT contract registered for this address
+        if (address(tokenContract) == address(0)) {
+            return "";
+        }
+
+        // Extract tokenId from child name
+        (bool valid, uint256 tokenId) = _parseTokenIdFromName(childUtf8Encoded);
+        if (!valid) {
+            return "";
+        }
+
+        // Don't bother returning anything if this tokenId has never been minted
+        try tokenContract.ownerOf(tokenId) {} catch {
+            return "";
+        }
+
+        if (key.equal("avatar")) {
+            // Standard described here:
+            // https://docs.ens.domains/ens-improvement-proposals/ensip-12-avatar-text-records
+            return string.concat("eip155:1/erc721:", address(tokenContract).toHexString(), "/", tokenId.toString());
+        }
+
+        // unsupported key
+        return "";
     }
 
     // TODO: move to separate library and unit test
@@ -65,4 +136,14 @@ contract Erc721WildcardResolver is IExtendedResolver, ERC165 {
         }
         return (true, tokenId);
     }
+
+    // solhint-disable
+    // Source: https://github.com/ensdomains/ens-contracts/blob/340a6d05cd00d078ae40edbc58c139eb7048189a/contracts/resolvers/profiles/AddrResolver.sol#L96
+    function addressToBytes(address a) internal pure returns (bytes memory b) {
+        b = new bytes(20);
+        assembly {
+            mstore(add(b, 32), mul(a, exp(256, 12))) // cspell:disable-line
+        }
+    }
+    // solhint-enable
 }
--- test/unit/ensWildcardResolvers/Erc721WildcardResolver.ts
+++ test/unit/ensWildcardResolvers/Erc721WildcardResolver.ts
@@ -1,18 +1,30 @@
 import { expect } from "chai";
 import { deployments, ethers, getNamedAccounts, getUnnamedAccounts } from "hardhat";
 
+import type { Erc721WildcardResolver, TestERC721 } from "../../../types";
 import {
-  type Erc721WildcardResolver,
   Erc721WildcardResolver__factory,
-  type TestERC721,
+  IAddrResolver__factory,
+  IAddressResolver__factory,
+  IPubkeyResolver__factory,
+  ITextResolver__factory,
   TestERC721__factory,
 } from "../../../types";
 import { getSigner } from "../../utils";
 
 export function testErc721WildcardResolver(): void {
-  describe("Erc721WildcardResolver", function () {
+  describe.only("Erc721WildcardResolver", function () {
     let resolver: Erc721WildcardResolver;
     let tokenContract: TestERC721;
+    let tokenOwner: string;
+
+    const ensParentName = "test.eth";
+    const tokenId = 123;
+    const fullName = `${tokenId}.${ensParentName}`;
+    const fullNameBytes = ethers.utils.dnsEncode(fullName);
+    const addrResolverIface = IAddrResolver__factory.createInterface();
+    const addressResolverIface = IAddressResolver__factory.createInterface();
+    const textResolverIface = ITextResolver__factory.createInterface();
 
     beforeEach("setup", async function () {
       await deployments.fixture();
@@ -32,21 +44,142 @@ export function testErc721WildcardResolver(): void {
         args: ["TestToken", "TEST"],
       });
       tokenContract = TestERC721__factory.connect(tokenDeployment.address, signer);
-    });
 
-    it("resolve - happy path", async function () {
-      const ensParentName = "test.eth";
-      const [tokenOwner] = await getUnnamedAccounts();
-      const tokenId = 123;
-
+      [tokenOwner] = await getUnnamedAccounts();
       await resolver.setTokenContract(ensParentName, tokenContract.address);
       await tokenContract.mint(tokenOwner, tokenId);
+    });
 
-      const fullNameBytes = ethers.utils.dnsEncode(`${tokenId}.${ensParentName}`);
+    describe("address records", function () {
+      it("resolves addr(bytes32 node) records", async function () {
+        const data = addrResolverIface.encodeFunctionData("addr", [ethers.utils.namehash(fullName)]);
 
-      const resolveResult = await resolver.resolve(fullNameBytes, []);
-      const [addr] = ethers.utils.defaultAbiCoder.decode(["address"], resolveResult);
-      expect(addr.toLowerCase()).to.eq(tokenOwner.toLowerCase());
+        const resolveResult = await resolver.resolve(fullNameBytes, data);
+        const [addr] = ethers.utils.defaultAbiCoder.decode(["address"], resolveResult);
+        expect(addr.toLowerCase()).to.eq(tokenOwner.toLowerCase());
+      });
+
+      it("resolves addr(bytes32 node, uint256 coinType) records", async function () {
+        const data = addressResolverIface.encodeFunctionData("addr", [
+          ethers.utils.namehash(fullName),
+          60 /* COINTYPE_ETH */,
+        ]);
+
+        const resolveResult = await resolver.resolve(fullNameBytes, data);
+        const [addr] = ethers.utils.defaultAbiCoder.decode(["bytes"], resolveResult);
+        expect(addr.toLowerCase()).to.eq(tokenOwner.toLowerCase());
+      });
+
+      it("returns zero address when subdomain not found", async function () {
+        const data = addrResolverIface.encodeFunctionData("addr", [ethers.utils.namehash(fullName)]);
+
+        const name = ethers.utils.dnsEncode(`${tokenId}.unregistered-domain.eth`);
+
+        const resolveResult = await resolver.resolve(name, data);
+        const [addr] = ethers.utils.defaultAbiCoder.decode(["address"], resolveResult);
+        expect(addr).to.eq(ethers.constants.AddressZero);
+      });
+
+      it("returns zero address when token has no owner", async function () {
+        const data = addrResolverIface.encodeFunctionData("addr", [ethers.utils.namehash(fullName)]);
+
+        const name = ethers.utils.dnsEncode(`99999.${ensParentName}`);
+
+        const resolveResult = await resolver.resolve(name, data);
+        const [addr] = ethers.utils.defaultAbiCoder.decode(["address"], resolveResult);
+        expect(addr).to.eq(ethers.constants.AddressZero);
+      });
+
+      it("returns zero address malformed token ID", async function () {
+        const data = addrResolverIface.encodeFunctionData("addr", [ethers.utils.namehash(fullName)]);
+
+        const name = ethers.utils.dnsEncode(`notAnInteger.${ensParentName}`);
+
+        const resolveResult = await resolver.resolve(name, data);
+        const [addr] = ethers.utils.defaultAbiCoder.decode(["address"], resolveResult);
+        expect(addr).to.eq(ethers.constants.AddressZero);
+      });
+
+      it("returns empty bytes for non-ETH addr() call", async function () {
+        const data = addressResolverIface.encodeFunctionData("addr", [
+          ethers.utils.namehash(fullName),
+          61 /* COINTYPE_ETH_CLASSIC */,
+        ]);
+
+        const resolveResult = await resolver.resolve(fullNameBytes, data);
+        const [result] = ethers.utils.defaultAbiCoder.decode(["bytes"], resolveResult);
+        expect(result).to.eq("0x");
+      });
+    });
+
+    describe("text records", function () {
+      it("resolves avatar URL from token URL", async function () {
+        const data = textResolverIface.encodeFunctionData("text", [ethers.utils.namehash(fullName), "avatar"]);
+        const resolveResult = await resolver.resolve(fullNameBytes, data);
+        const [observed] = ethers.utils.defaultAbiCoder.decode(["string"], resolveResult);
+
+        const expected = `eip155:1/erc721:${tokenContract.address.toLowerCase()}/${tokenId}`;
+
+        expect(observed).to.eq(expected);
+      });
+
+      it("returns empty string when subdomain not found", async function () {
+        const data = textResolverIface.encodeFunctionData("text", [ethers.utils.namehash(fullName), "avatar"]);
+
+        const name = ethers.utils.dnsEncode(`${tokenId}.unregistered-domain.eth`);
+
+        const resolveResult = await resolver.resolve(name, data);
+        const [value] = ethers.utils.defaultAbiCoder.decode(["string"], resolveResult);
+        expect(value).to.eq("");
+      });
+
+      it("returns empty string when token has no owner", async function () {
+        const data = textResolverIface.encodeFunctionData("text", [ethers.utils.namehash(fullName), "avatar"]);
+
+        const name = ethers.utils.dnsEncode(`99999.${ensParentName}`);
+
+        const resolveResult = await resolver.resolve(name, data);
+        const [value] = ethers.utils.defaultAbiCoder.decode(["string"], resolveResult);
+        expect(value).to.eq("");
+      });
+
+      it("returns empty string malformed token ID", async function () {
+        const data = textResolverIface.encodeFunctionData("text", [ethers.utils.namehash(fullName), "avatar"]);
+
+        const name = ethers.utils.dnsEncode(`notAnInteger.${ensParentName}`);
+
+        const resolveResult = await resolver.resolve(name, data);
+        const [value] = ethers.utils.defaultAbiCoder.decode(["string"], resolveResult);
+        expect(value).to.eq("");
+      });
+    });
+
+    it("reverts for unsupported record lookups", async function () {
+      const pubkeyResolverIface = IPubkeyResolver__factory.createInterface();
+      const data = pubkeyResolverIface.encodeFunctionData("pubkey", [ethers.utils.namehash(fullName)]);
+
+      const resolveCall = resolver.resolve(fullNameBytes, data);
+      await expect(resolveCall).to.be.revertedWithCustomError(resolver, "RecordTypeNotSupported");
+    });
+
+    describe("has correct function selector constants", function () {
+      it("addr(bytes32 node)", async function () {
+        const expected = addrResolverIface.getSighash(addrResolverIface.functions["addr(bytes32)"]);
+        const observed = await resolver.RESOLVER_SIGNATURE__ADDR();
+        expect(observed).to.eq(expected);
+      });
+
+      it("addr(bytes32 node, uint256 cointype)", async function () {
+        const expected = addressResolverIface.getSighash(addressResolverIface.functions["addr(bytes32,uint256)"]);
+        const observed = await resolver.RESOLVER_SIGNATURE__ADDR_WITH_COINTYPE();
+        expect(observed).to.eq(expected);
+      });
+
+      it("text(bytes32 node, string key)", async function () {
+        const expected = textResolverIface.getSighash(textResolverIface.functions["text(bytes32,string)"]);
+        const observed = await resolver.RESOLVER_SIGNATURE__TEXT();
+        expect(observed).to.eq(expected);
+      });
     });
   });
 }
