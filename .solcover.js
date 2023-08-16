62d09be8287ad462f64bd3598e16d8f1afda5b2f
Kevin Ayling <kra4ck@virginia.edu>
2023-08-15 17:02:01 -0700

    ci: skip-on-coverage tag config

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,9 @@
 module.exports = {
   istanbulReporter: ["html", "lcov"],
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true, // Run the grep's inverse set.
+  },
   providerOptions: {
     mnemonic: process.env.MNEMONIC,
   },
